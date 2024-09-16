2d52add94bd6f784cc19e9ae826de3c8dbefc7ee
Kevin Sheeran <108253891+KevinSheeranxyj@users.noreply.github.com>
2024-09-16 22:20:59 +0800

    Delete backend directory

--- backend/model/walrus_model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type BlobUploadResponse struct {
-	NewlyCreated struct {
-		BlobObject struct {
-			BlobID string `json:"blob_id"`
-		} `json:"blob_object"`
-	} `json:"newly_created"`
-}
